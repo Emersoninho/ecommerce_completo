4429ff2a2ce820458b102a797072c3ca22ac3ec2
emerson <emersoneletrotecnico2013@hotmail.com>
2025-04-21 00:12:29 -0300

    ecommerce/urls.py,    products/migrations/0005_product_active_product_featured.py, products/models.py,    products/templates/products/detail.html, products/templates/products/featured-detail.html, and  products/views.py

--- products/models.py
+++ products/models.py
@@ -1,17 +1,36 @@
 from django.db import models
 
+# custom queryset
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
 class ProductMenager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self): 
+        return self.get_queryset().active()
+
+    def featured(self):
+        return self.get_queryset().featured()
+
     def get_by_id(self, id):
         qs = self.get_queryset().filter(id=id)
         if qs.count() == 1:
             return qs.first()
         return None
-    
+
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(max_digits=20, decimal_places=2, default=100.00)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
+    featured = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
 
     objects = ProductMenager()
     
--- products/views.py
+++ products/views.py
@@ -1,36 +1,51 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Product
 from django.http import Http404
 
-# class Based View
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+    
+    def get_queryset(self, *args, **kwargs):
+        return Product.objects.featured()
+
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = "products/featured-detail.html"
+
+    #def get_queryset(self, *args, **kwargs):
+        #request = self.request
+        #return Product.objects.featured()
+
+#Class Based View
 class ProductListView(ListView):
-    #traz todos os produtos do banco de dados sem filtrar nada
+    #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
-    template_name = 'products/list.html'
+    template_name = "products/list.html"
 
     # def get_context_data(self, *args, **kwargs):
     #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
     #     print(context)
     #     return context
 
-#Funcion Based View
+
+#Function Based View
 def product_list_view(request):
     queryset = Product.objects.all()
     context = {
         'object_list': queryset
-    }    
-    return render(request, 'products/list.html', context)
+    }
+    return render(request, "products/list.html", context)
 
-#class Based View
-class ProductDetailView(DeleteView):
-    template_name = 'products/detail.html'
+#Class Based View
+class ProductDetailView(DetailView):
+    template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
-        return 
-    
+        return context
+
     def get_object(self, *args, **kwargs):
         pk = self.kwargs.get('pk')
         instance = Product.objects.get_by_id(pk)
@@ -38,17 +53,14 @@ class ProductDetailView(DeleteView):
             raise Http404("Esse produto não existe!")
         return instance
 
-#Funcion Based View
-def product_detail_view(request, pk=None, *args, **kwargs):
-    #instance = Product.objects.get(pk=pk) 
-    #instance = Product.objects.get_by_id(pk)
-    qs = Product.objects.filter(id=pk)
-    if qs.count() == 1:
-        instance = qs.first()
-    else:
-        raise Http404('Esse produto não exixte!')     
-    
+#Function Based View
+def product_detail_view(request, pk = None, *args, **kwargs):
+    instance = Product.objects.get_by_id(pk)
+    print(instance)
+    if instance is None:
+        raise Http404("Esse produto não existe!")
+
     context = {
         'object': instance
     }
-    return render(request, 'products/detail.html', context)
+    return render(request, "products/detail.html", context)
