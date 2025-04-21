f7bd1982bcac8781be236b72f19a60aaf15532f5
emerson <emersoneletrotecnico2013@hotmail.com>
2025-04-20 21:30:15 -0300

    products/models.py and  products/views.py

--- products/models.py
+++ products/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class ProductMenager(models.Manager):
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+    
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(max_digits=20, decimal_places=2, default=100.00)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
 
+    objects = ProductMenager()
+    
     def __str__(self):
         return self.title
--- products/views.py
+++ products/views.py
@@ -24,25 +24,30 @@ def product_list_view(request):
 
 #class Based View
 class ProductDetailView(DeleteView):
-    #traz um produto específico do banco de dados
-    queryset = Product.objects.all()
     template_name = 'products/detail.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
-        return context
+        return 
+    
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
 
 #Funcion Based View
 def product_detail_view(request, pk=None, *args, **kwargs):
-    #instance = Product.objects.get(pk=pk) #get the object id
-    #instance = get_object_or_404(Product, pk=pk)
+    #instance = Product.objects.get(pk=pk) 
+    #instance = Product.objects.get_by_id(pk)
     qs = Product.objects.filter(id=pk)
     if qs.count() == 1:
         instance = qs.first()
     else:
-        raise Http404("Esse produto não existe!")
-
+        raise Http404('Esse produto não exixte!')     
+    
     context = {
         'object': instance
     }
