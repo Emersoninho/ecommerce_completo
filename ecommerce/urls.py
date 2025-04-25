97d5380a4f9f1fe9e80ca7cee1051f55802ab55e
emerson <emersoneletrotecnico2013@hotmail.com>
2025-04-25 14:57:04 -0300

    ecommerce/urls.py,  ecommerce/views.py, templates/auth/logout.html and templates/base/navbar.html

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,14 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_page, about_page, contact_page, login_page, register_page
+from .views import home_page, about_page, contact_page, login_page, logout_page, register_page
 
 urlpatterns = [
-    path('', home_page),
-    path('about/', about_page),
-    path('contact/', contact_page),
-    path('login/', login_page),
-    path('register/', register_page),
+    path('', home_page, name='home'),
+    path('about/', about_page, name='about'),
+    path('contact/', contact_page, name='contatc'),
+    path('login/', login_page, name='login'),
+    path('logout/', logout_page, name='logout'),
+    path('register/', register_page, name='register'),
     path('products/', include('products.urls', namespace='products')),
     path('admin/', admin.site.urls),
 ]
