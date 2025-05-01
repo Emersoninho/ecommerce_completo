80cadb57d46eec31b2cd8244485ae55fad99bd0a
emerson <emersoneletrotecnico2013@hotmail.com>
2025-05-01 17:17:45 -0300

    search/templates/search/snippets/search-form.html,  search/templates/search/view.html, search/urls.py, search/views.py and  templates/base/navbar.html

--- search/urls.py
+++ search/urls.py
@@ -4,7 +4,7 @@ from.views import SearchProductView
 app_name = 'search'
 
 urlpatterns = [
-    path('', SearchProductView.as_view(), name='list')
+    path('', SearchProductView.as_view(), name='query')
 ]
 
 
