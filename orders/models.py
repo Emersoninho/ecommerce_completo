9c273e398169e1d7fca7308887ddd45d5fd11d78
emerson <emersoneletrotecnico2013@hotmail.com>
2025-05-22 22:21:22 -0300

    carts/views.py and  orders/models.py

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,9 @@
 import math
 from django.db import models
 from django.db.models.signals import pre_save, post_save
+from billing.models import BillingProfile
 from carts.models import Cart
 from ecommerce.utils import unique_order_id_generator
-from billing.models import BillingProfile
 
 ORDER_STATUS_CHOICES = (
     ('created', 'Criado'),
@@ -11,9 +11,24 @@ ORDER_STATUS_CHOICES = (
     ('shipped', 'Enviado'),
     ('refunded', 'Devolvido'),
 )
+class OrderManager(models.Manager):
+    def new_or_get(self, billing_profile, cart_obj):
+        created = False
+        qs = self.get_queryset().filter(
+                billing_profile = billing_profile, 
+                cart = cart_obj, 
+                active = True)
+        if qs.count() == 1:
+            obj = qs.first()
+        else:
+            obj = self.model.objects.create(
+                    billing_profile = billing_profile, 
+                    cart=cart_obj)
+            created = True
+        return obj, created
 
 class Order(models.Model):
-    billing_profile = models.ForeignKey(BillingProfile, on_delete=models.CASCADE, null=True, blank=True)
+    billing_profile = models.ForeignKey(BillingProfile, on_delete=models.CASCADE, null = True, blank = True)
     order_id = models.CharField(max_length = 120, blank = True)
     # billing_profile = ?
     # shipping_address = ?
@@ -26,19 +41,26 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_id
+
+    objects = OrderManager()
+
     def update_total(self):
         cart_total = self.cart.total
         shipping_total = self.shipping_total
-        #new_total = cart_total + shipping_total
         new_total = math.fsum([cart_total, shipping_total])
         formatted_total = format(new_total, '.2f')
-        self.total = formatted_total
+        self.total = formatted_total 
         self.save()
         return new_total
 
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
+    # retorna todos os orders com esta instância de cart, 
+    # excluindo aqueles que têm a mesma instância de billing_profile
+    qs = Order.objects.filter(cart = instance.cart).exclude(billing_profile = instance.billing_profile)
+    if qs.exists():
+        qs.update = False
 
 pre_save.connect(pre_save_create_order_id, sender = Order)
 
