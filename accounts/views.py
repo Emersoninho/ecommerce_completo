6eea2c77e1f869db6c86f59789f3f6b740667c1b
emerson <emersoneletrotecnico2013@hotmail.com>
2025-05-22 21:42:26 -0300

    accounts/views.py,  carts/templates/carts/checkout.html,  carts/views.py,  orders/migrations/0003_order_active_order_billing_profile.py and  orders/models.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,10 @@ from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.utils.http import url_has_allowed_host_and_scheme
+
 from .forms import LoginForm, RegisterForm, GuestForm
 from .models import GuestEmail
+
 
 def guest_register_view(request):
     form = GuestForm(request.POST or None)
@@ -15,7 +17,7 @@ def guest_register_view(request):
     redirect_path = next_ or next_post or None
     if form.is_valid():
         email       = form.cleaned_data.get("email")
-        new_guest_email = GuestEmail.objects.create(email = email)
+        new_guest_email = GuestEmail.objects.create(email=email)
         request.session['guest_email_id'] = new_guest_email.id
         if url_has_allowed_host_and_scheme(redirect_path, request.get_host()):
             return redirect(redirect_path)
@@ -28,21 +30,19 @@ def login_page(request):
     context = {
                     "form": form
               }
-    print("User logged in")
-    print(request.user.is_authenticated)
     next_ = request.GET.get('next')
     next_post = request.POST.get('next')
-    redirect_path = next_ or next_post or None    
+    redirect_path = next_ or next_post or None
     if form.is_valid():
-        print(form.cleaned_data)
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password")
         user = authenticate(request, username=username, password=password) 
-        print(user)
-        print(request.user.is_authenticated)
         if user is not None:
-            print(request.user.is_authenticated)
             login(request, user)
+            try:
+                del request.session['guest_email_id']
+            except:
+                pass
             if url_has_allowed_host_and_scheme( redirect_path, request.get_host() ):
                 return redirect( redirect_path )
             else:
@@ -64,7 +64,7 @@ User = get_user_model()
 def register_page(request):
     form = RegisterForm(request.POST or None)
     context = {
-                    "form": form
+                "form": form
               }
     if form.is_valid():
         print(form.cleaned_data)
@@ -73,4 +73,4 @@ def register_page(request):
         password = form.cleaned_data.get("password")
         new_user = User.objects.create_user(username, email, password)
         print(new_user)
-    return render(request, "accounts/register.html", context) 
+    return render(request, "accounts/register.html", context)
